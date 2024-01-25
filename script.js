41ee1d7015928fdfbecf33b560cb4ff3686b4372
s12imn <157729290+s12imn@users.noreply.github.com>
2024-01-25 18:48:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function changeGreeting() {
             greeting.textContent = 'I want to tell you something';
             break;
         case 'I want to tell you something':
-            greeting.textContent = 'you are the source of my light';
+            greeting.textContent = 'I believe that distance is just a number, it is a bliss for us and a test to show us that every single hard situation is easier when we look at it in different point of view, and I started to believe even more that by our will to be together in life and with our deepest love, we will be the greatest support to each other even with this distance and our lives will walk in the directions of bringing us together';
             break;
-        case 'you are the source of my light':
-            greeting.textContent = 'and I am grateful to have you in my life';
+        case 'I believe that distance is just a number, it is a bliss for us and a test to show us that every single hard situation is easier when we look at it in different point of view, and I started to believe even more that by our will to be together in life and with our deepest love, we will be the greatest support to each other even with this distance and our lives will walk in the directions of bringing us together':
+            greeting.textContent = 'this ditance relationship makes us value each other so much and makes us building a very strong trust more than we can ever imagine. Each time I walk back to my apartment after being with you, I feel like the walk is so hard like there's a power puling me back tying my legs and my arms not allowing me to walk forward the gate, a power stronger than thgravity pulling me to your direction to your arms!!';
             break;
         default:
-            greeting.textContent = 'Hello my love'; // Reset to the initial greeting if none of the cases match
+            greeting.textContent = 'I love you'; // Reset to the initial greeting if none of the cases match
     }
 }
