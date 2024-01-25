71d4b154f0285a6d44306c7c68db300be177b3d8
s12imn <157729290+s12imn@users.noreply.github.com>
2024-01-25 18:27:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function changeGreeting() {
     var currentText = greeting.textContent;
 
     if (currentText === 'Hello my love') {
-        greeting.textContent = 'I want to tell you something'
-    } else if (currentText === 'I want to tell you something') {
-        greeting.textContent = 'you are the source of my light'
-    } else {
+        greeting.textContent = 'I want to tell you something'}
+    else if (currentText === 'I want to tell you something') {
+        greeting.textContent = 'you are the source of my light' } 
+    else {
         greeting.textContent = 'and I am grateful to have you in my life'
     }
 }
