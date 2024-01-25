75524b2d8a6c28f6c1061b86aa4af138cbe67e5a
s12imn <157729290+s12imn@users.noreply.github.com>
2024-01-25 18:30:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function changeGreeting() {
     var greeting = document.querySelector('h1');
     var currentText = greeting.textContent;
 
-    if (currentText === 'Hello my love') {
-        greeting.textContent = 'I want to tell you something';
-    } else if (currentText === 'I want to tell you something') {
-        greeting.textContent = 'you are the source of my light'; 
-    } else if (currentText === 'you are the source of my light') {
-        greeting.textContent = 'and I am grateful to have you in my life';
+    switch (currentText) {
+        case 'Hello my love':
+            greeting.textContent = 'I want to tell you something';
+            break;
+        case 'I want to tell you something':
+            greeting.textContent = 'you are the source of my light';
+            break;
+        case 'you are the source of my light':
+            greeting.textContent = 'and I am grateful to have you in my life';
+            break;
+        default:
+            greeting.textContent = 'Hello my love'; // Reset to the initial greeting if none of the cases match
     }
 }
